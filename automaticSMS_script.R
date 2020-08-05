0785945ba5926a5af0a40f2f8bf332690d2ccf92
Yannis Chourdakis <yannis.chourdakis@gmail.com>
2020-08-05 12:28:28 +0300

    Update automaticSMS_script.R

--- automaticSMS_script.R
+++ automaticSMS_script.R
@@ -7,8 +7,8 @@ library(shinyFiles)
 
 
 # First you need to set up your accound SID and token as environmental variables
-Sys.setenv(TWILIO_SID = "AC6402e4dd7dc36303e61615be88c85feb")
-Sys.setenv(TWILIO_TOKEN = "16dae77210501324c36202d4bce72186")
+Sys.setenv(TWILIO_SID = "****02e4dd7dc36303e61615be88c85feb")
+Sys.setenv(TWILIO_TOKEN = "****e77210501324c36202d4bce72186")
 # https://stackoverflow.com/questions/20577764/set-locale-to-system-default-utf-8
 # https://crontab.guru/#58_12_*_*_1-6
 Sys.getenv('LANG')
@@ -16,14 +16,12 @@ Sys.setlocale("LC_ALL","el_GR")
 Sys.setenv(LANG="el_GR")
 
 # Then we're just going to store the numbers in some variables
-twilios_phone_number <- "+13156424484"
+twilios_phone_number <- "+13156424***"
 # Now we can send away!
-#tw_send_message(from = twilios_phone_number, to = "+447873886652", 
-                #body = paste0("Υπενθύμιση για έλεγχο αποθεμάτων καυσίμων. Ενημέρωσε την Φωτεινή για να υπολογίσει την επόμενη παραγγελία", ji('gas')))
-tw_send_message(from = twilios_phone_number, to = "+306972200464", 
-                body = paste0("Υπενθύμιση για έλεγχο αποθεμάτων καυσίμων. Ενημέρωσε την Φωτεινή για να υπολογίσει την επόμενη παραγγελία", ji('gas')))
-tw_send_message(from = twilios_phone_number, to = "+306944797419", 
-                body = paste0("Υπενθύμιση για έλεγχο αποθεμάτων καυσίμων. Ενημέρωσε την Φωτεινή για να υπολογίσει την επόμενη παραγγελία", ji('gas')))
+tw_send_message(from = twilios_phone_number, to = "+30**********", 
+                body = paste0("Hello world!", ji('gas')))
+tw_send_message(from = twilios_phone_number, to = "+30**********", 
+                body = paste0("Hello world", ji('gas')))
 
 
 
